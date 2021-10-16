5fc5fe95888c9b802c68dc4ce0328a4a79f25b54
cyril <jesuisledelegue@gmail.com>
2021-10-16 13:42:39 +0200

    Fini de la fonction stringToArray, on renvoie en fait un Objet js

--- lib.js
+++ lib.js
@@ -1,8 +1,15 @@
 const data = require("./valeurs");
 const puppeteer = require('puppeteer');
 
+
+/**
+ * Fonction qui récupére un Array de string pour le transformer en Array de JSON
+ * Si le cours est le dernier de la journée, le flag #END# se trouve collé au Nom du prof
+ * @param string
+ * @returns {{Events: *[]}} JSON type {"salle" : '',"cours" : '', "debut" : '', "fin" : '', "prof" : ''}
+ */
 let stringToArray = (string) => {
-    let retour = [];
+    let retour = {"Events" : []};
     string.forEach(line => {
         // Si la ligne n'est pas du type " - ISEN ......"
         // Elle est trop dure à traiter du coup je fais ce que je peux càd je prends heure, prof, et je garde le reste en texte
@@ -27,7 +34,7 @@ let stringToArray = (string) => {
             //line = line.trim();
             let cours = line;
 
-            retour.push(['', line, debut, fin, prof]);
+            retour['Events'].push({"salle" : '', "cours" : line, "debut" :debut, "fin" : fin, "prof" : prof});
 
 
         }else{ // Si la ligne est du type " - ISEN ...."
@@ -51,11 +58,39 @@ let stringToArray = (string) => {
 
             let cours = line;
 
-            retour.push([salle[0], cours, debut, fin, prof]);
+            retour['Events'].push({"salle" : salle[0], "cours" : line, "debut" :debut, "fin" : fin, "prof" : prof});
+
         }
     });
     return retour;
 }
 
-//stringToArray(data.events);
-console.log(stringToArray(data.events));
+let daterEvents = (events, dateJours, dateSemaine) => {
+    // Chaque jour avec un 0 pour journée qui continue et un 1 pour journée fini (le samedi est toujours à 1 CIR ZOO)
+    let fini = [0, 0, 0, 0, 0, 1];
+    let nbSemaine = 0;
+    let retour = {"Semaine" : []};
+
+    // On crée le JSON avec les  6 semaines. C'était pas facile à prendre en main. Mais dès que tu as le truc c'es simple
+    for (let i = 0; i < dateSemaine.length; i++){
+        retour["Semaine"][i] =
+            {
+                "nbemaine": dateSemaine[i],
+                "Lundi": [],
+                "Mardi": [],
+                "Mercredi": [],
+                "Jeudi": [],
+                "Vendredi": [],
+                "Samedi": [],
+
+            };
+    }
+    return retour;
+}
+
+//Cette ligne crée un objet JSON
+//let test = JSON.stringify(stringToArray(data.events));
+let test = stringToArray(data.events);
+//console.log(test.Events);
+let semaine = daterEvents(test, data.nbDate, data.nbSemaine).Semaine;
+console.log(semaine[0].Jeudi);
