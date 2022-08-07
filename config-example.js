fcce6a7db2469a95486a2a08bc0322b826eaa90e
Martin Delebecque <martin-delebecque@outlook.fr>
2022-08-07 18:52:30 +0100

    Update the config system, nothing important

--- config-example.js
+++
@@ -1,9 +0,0 @@
-const user = "prenom.nom@student.junia.com"; // Adresse mail junia
-const password = "mdp"; // mot de passe
-const path = "/path/where/you/want/name.ics"; // L'endroit où sauvegarder le fichier "name.ics"
-
-module.exports = {
-    user,
-    password,
-    path
-};
